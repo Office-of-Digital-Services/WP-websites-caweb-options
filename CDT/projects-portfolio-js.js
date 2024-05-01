e645d71de875e2b28aee13ff9a71a8ec56799cc4
Carter Medlin <carterm@users.noreply.github.com>
2024-05-01 13:54:30 -0700

    custom commands moved to file

--- CDT/projects-portfolio-js.js
+++ CDT/projects-portfolio-js.js
@@ -8,10 +8,12 @@ function showMoreInfo(buttonElement) {
 
   //checking the "row-" class
   let rowIndex =
-    $(buttonElement)
-      .closest("tr")
-      .attr("class")
-      .match(/row-(\d+)/)[1] - 2;
+    Number(
+      $(buttonElement)
+        .closest("tr")
+        .attr("class")
+        .match(/row-(\d+)/)[1]
+    ) - 2;
   let rowData = table.row(rowIndex).data();
 
   let content = `
@@ -64,4 +66,99 @@ document.body.onkeydown = function () {
   }
 };
 
+// eslint-disable-next-line no-unused-vars
+function TablePressCustomCommands() {
+  //Place this in "Custom Commands"
+  // https://cdt.ca.gov/wp-admin/admin.php?page=tablepress&action=edit&table_id=86
+  return {
+    search: {
+      exact: true,
+      smart: false
+    },
+    columns: [
+      { data: "Agency Name" },
+      {
+        data: "Department Name"
+      },
+      {
+        data: "Project Name"
+      },
+      {
+        data: "Total Cost",
+        defaultContent: "<em>N/A</em>",
+        render: function (data, type) {
+          if (type === "display") {
+            if (!data) return null; //shows defaultContent
 
+            return Number(data).toLocaleString(navigator.language || "en-US", {
+              style: "currency",
+              currency: "USD",
+              minimumFractionDigits: 0
+            });
+          } else if (type === "sort") {
+            if (!data) {
+              return "-1"; //puts N/A before $0
+            }
+          }
+
+          return data;
+        }
+      },
+      {
+        data: "Criticality Level",
+        name: "Criticality Level",
+        className: "column_criticality_level",
+        render: function (data, type) {
+          const values = {
+            High: { sort: 3, color: "#f2b23e" },
+            Medium: { sort: 2, color: "#1a3e62" },
+            Low: { sort: 1, color: "#4590ca" },
+            Delegated: { sort: 0, color: "#666666" }
+          };
+          if (type === "display") {
+            return `<div class='mt-1 float-start height-20 width-20 m-r-sm' style='background-color: ${values[data].color}'></div> ${data}`;
+          } else if (type === "sort") {
+            const found = values[data];
+            if (found) {
+              return found.sort.toString();
+            } else return "-1";
+          }
+          return data;
+        }
+      },
+      {
+        data: "Description",
+        name: "Description",
+        searchable: true,
+        visible: false
+      },
+      {
+        data: "Condition",
+        className: "column_condition",
+        defaultContent: "<em>N/A</em>",
+        render: function (data, type) {
+          if (type === "display") {
+            if (!data) return null; //shows defaultContent
+
+            return data
+              .replace(/Red/, "🔴 Red")
+              .replace(/Green/, "🟢 Green")
+              .replace(/Yellow/, "🟡 Yellow");
+          } else if (type === "sort") {
+            switch (data) {
+              case "Red":
+                return "3";
+              case "Yellow":
+                return "2";
+              case "Green":
+                return "1";
+              default:
+                return "-1";
+            }
+          }
+          return data;
+        }
+      }
+    ]
+  };
+}
