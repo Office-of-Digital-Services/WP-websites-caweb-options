5b8a4ff3b9ee51cae4a7b9d11587d33810d726fd
Carter Medlin <carterm@users.noreply.github.com>
2024-05-02 11:37:28 -0700

    code improvments

--- CDT/projects-portfolio-js.js
+++ CDT/projects-portfolio-js.js
@@ -1,3 +1,4 @@
+//@ts-check
 /**
  * display popover
  * @param {JQuery<HTMLElement>} buttonElement
@@ -16,55 +17,63 @@ function showMoreInfo(buttonElement) {
     ) - 2;
   let rowData = table.row(rowIndex).data();
 
-  let content = `
+  const content = `
   <dt>Project Name</dt><dd>${rowData["Project Name"]}</dd>
   <dt>Description</dt><dd>${rowData.Description}</dd>
+  <button class="btn btn-secondary wp-block-button__link">Close</button>
   `;
-
-  content +=
-    '<button class="btn btn-secondary wp-block-button__link">Close</button>';
 
   $("#fundingPopover").remove();
   row.append('<div id="fundingPopover"></div>');
   $("#fundingPopover").append(content); // insert new content
 
-  jQuery("button.btn").click(function () {
+  $("button.btn").on("click", function () {
     $("#fundingPopover").remove();
   });
 
   // don't close when user clicks on popover content
-  jQuery("#fundingPopover").click(function (ev) {
+  $("#fundingPopover").on("click", function (ev) {
     ev.stopPropagation();
   });
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
+  /**
+   *
+   * @param {JQuery<HTMLElement>} $element
+   * @param {JQuery.Event} e
+   */
+  function showHandler($element, e) {
+    showMoreInfo($element);
+    e.preventDefault();
+    e.stopPropagation(); // stop click propogation
+  }
+
   // insert "More details" link
   $("#funding_container td.column-3")
     .append('<p class="MoreInfoButton" tabindex="0">More details</p>')
-    .click(function (e) {
-      showMoreInfo($(this));
-      e.stopPropagation(); // stop click propogation
+    .on("click", function (e) {
+      showHandler($(this), e);
     })
-    .keydown(function (e) {
-      if (event.code === "Space" || event.code === "Enter") {
-        showMoreInfo($(this));
-        e.stopPropagation(); // stop click propogation
+    .on("keydown", function (e) {
+      if (e.key === " " || e.key === "Enter") {
+        // " " is "Space"
+        showHandler($(this), e);
       }
     });
 });
 
 // close popover when clicking anywhere
-window.onclick = function () {
-  $("#fundingPopover").remove();
-};
 
-// close popover when user hits esc
-document.body.onkeydown = function () {
-  if (event.code === "Escape") {
+window.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
     $("#fundingPopover").remove();
   }
-};
+});
+
+window.addEventListener("click", function () {
+  $("#fundingPopover").remove();
+});
 
 /**
  * Put this in the "Custom Commands" field in the table settings
